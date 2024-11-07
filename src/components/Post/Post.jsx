fd0b8f53ae5425eb5f57215bfe720fb0a0b178a6
ghstll <129940439+ghstll@users.noreply.github.com>
2024-11-06 19:47:06 -0600

    post structure

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,11 +1,29 @@
 //  
-function Post({title,date,content,imgs}){
+import styles from "./Post.module.scss"
+function Post({title , samplecontent, imgsrc}){
+
+
+
     return(
         <>
+            <div className={styles["post-container"]}>
+                <header className={styles["titulo"]}>
+                    <h1>{title}</h1>
+                </header>
 
+                <p className={styles["sample-content"]}>{samplecontent}</p>
+                <div>
+                    <img className={styles["img-content"]} src={imgsrc} alt="" />
+                </div>
+                <hr />
+            </div>
         </>
     )
-}
 
+    
+}
+Post.defaultProps = {
+    imgsrc : ""
+}
 
 export default Post
